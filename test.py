d03766aec6d7e2f3a651d4e804744174c8204907
Alice King <88600460+alice-king@users.noreply.github.com>
2021-10-23 23:08:43 +0800

    Test

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 from scapy.all import *
-pkg=IP(dst='195.133.10.119',ttl=255)/ICMP(type=11)/IPerror(src='195.133.10.119',dst='3.3.3.3',ttl=1)/ICMPerror()
-send(pkg)
+ans,unans=sr(IP(dst='3.3.3.3')/ICMP())
+ans[0][1].show()
