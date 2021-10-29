f5f3e5863429b7f7c4ede02d0bb7faaf4ce90f80
Alice King <88600460+alice-king@users.noreply.github.com>
2021-10-29 10:33:36 +0800

    Test

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
+import time
 import asyncio
 import websockets
 
 async def hello():
     async with websockets.connect("ws://lorddimension.com/ws") as websocket:
         print("Connected")
-        async for message in websocket:
-            await websocket.send(message)
+        start=time.time()
+        for i in range(100):
+            recv=await websocket.recv()
+        spend=time.time()-start
+        print("Received 100 messages in %.2f seconds" % spend)
+        print("Received %.2f messages per second" % (100/spend))
+        start=time.time()
+        for i in range(100):
+            await websocket.send("Hello, world!")
+        spend=time.time()-start
+        print("Sent 100 messages in %.2f seconds" % spend)
+        print("Sent %.2f messages per second" % (100/spend))
         print("Disconnected")
 
 asyncio.get_event_loop().run_until_complete(hello())
