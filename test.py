c81ac0c60c24f038d6b0e3f2eb880b66b63b05ce
Alice King <88600460+alice-king@users.noreply.github.com>
2021-10-29 10:24:35 +0800

    Test

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
-import time
 import asyncio
 import websockets
 
 async def hello():
     async with websockets.connect("ws://lorddimension.com/ws") as websocket:
-        for i in range(2000):
-            await websocket.send("Hello world!")
-            recv=await websocket.recv()
-            print(recv,i)
-            time.sleep(0.01)
+        print("Connected")
+        async for message in websocket:
+            await websocket.send(message)
+        print("Disconnected")
 
 asyncio.get_event_loop().run_until_complete(hello())
