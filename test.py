d61c41907c83aa9bc14240b5e296c8aca702aeee
Alice King <88600460+alice-king@users.noreply.github.com>
2021-11-27 21:44:35 +0800

    Test

--- test.py
+++ test.py
@@ -3,36 +3,40 @@ import asyncio
 import websockets
 
 async def hello(host):
-    async with websockets.connect(host,max_size=2**32) as websocket:
-    # async with websockets.connect("wss://api.lonedark.com",max_size=2**32) as websocket:
+    async with websockets.connect(host,max_size=2**32,,ping_interval=60) as websocket:
         print("Connected:"+str(websocket.local_address))
+
         recv=await websocket.recv()
         start=time.time()
         for i in range(1000):
             recv=await websocket.recv()
         spend=time.time()-start
+        await websocket.send(str(spend))
         print("Received 1000 messages in %.2f seconds" % spend)
         print("Received %.2f messages per second" % (1000/spend))
+
         await websocket.send("Hello, world!")
-        start=time.time()
         for i in range(1000):
             await websocket.send("Hello, world!")
-        spend=time.time()-start
+        spend=float(await websocket.recv())
         print("Sent 1000 messages in %.2f seconds" % spend)
         print("Sent %.2f messages per second" % (1000/spend))
+
         recv=await websocket.recv()
         start=time.time()
         recv=await websocket.recv()
         spend=time.time()-start
-        print("Received 10MB in %.2f seconds" % spend)
+        await websocket.send(str(spend))
+        print("Received 10MB in %.2f seconds" % spend,len(recv))
         print("Received %.2fMB per second" % (10/spend))
+
         await websocket.send("Hello, world!")
-        start=time.time()
-        await websocket.send(b'\x00'*10485760)
-        spend=time.time()-start
+        await websocket.send(bytes(10485760))
+        spend=float(await websocket.recv())
         print("Sent 10MB in %.2f seconds" % spend)
         print("Sent %.2fMB per second" % (10/spend))
+
         print("Disconnected")
 
-asyncio.get_event_loop().run_until_complete(hello("ws://localhost"))
-asyncio.get_event_loop().run_until_complete(hello("wss://api.lonedark.com"))
+asyncio.get_event_loop().run_until_complete(hello("ws://localhost/ws/"))
+asyncio.get_event_loop().run_until_complete(hello("wss://api.lonedark.ml/ws/"))
